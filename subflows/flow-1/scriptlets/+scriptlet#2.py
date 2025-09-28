3b88e964f982c446ee43dd6ddef9e827204de3ae
alwaysmavs <beingswu@gmail.com>
2025-09-28 06:09:08 +0000

    chore: update dependencies

---
+++ subflows/flow-1/scriptlets/+scriptlet#2.py
@@ -0,0 +1,13 @@
+from oocana import Context
+
+#region generated meta
+import typing
+Inputs = typing.Dict[str, typing.Any]
+Outputs = typing.Dict[str, typing.Any]
+#endregion
+
+def main(params: Inputs, context: Context) -> Outputs:
+
+    # your code
+
+    return {}
